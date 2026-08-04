a8f858d086686df74a1cc01bc282ada1f8120b5d
LohYangXian <e0726433@u.nus.edu>
2026-08-04 11:10:52 +0800

    did more cpp!

---
+++ Codeforces/1300/1077C.cpp
@@ -0,0 +1,72 @@
+//Good Array
+
+/*
+Good array if there is an element in the array
+that equals to sum of all other elements
+
+a = [1,3,3,7] is good if 
+elem a4 = 7 equals to sum of all other elements
+
+print all indices j of this array such that after removing
+j , it will be good
+
+you have to remove all independently 
+
+print k , number of indices j ,
+print k integers in any order
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> pre(n);
+    vector<long long> suf(n);
+    vector<long long> v{};
+
+    vector<int> answers{};
+
+    unordered_map<int, vector<int>> indices{};
+
+    for (int i = 1; i <= n; i++) {
+        long long x;
+        cin >> x;
+        indices[x].push_back(i);
+        v.push_back(x);
+    }
+
+    for (int i = 1; i < n; i++) {
+        pre[i] = (pre[i - 1] + v[i - 1]);
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        suf[i] = (suf[i + 1] + v[i + 1]);
+    }
+
+    long long res{};
+
+    for (int i = 0; i < n; i++) {
+        long long remaining = pre[i] + suf[i];
+        
+        if (remaining % 2 == 0) {
+            long long target = remaining / 2;
+
+            if (indices.find(target) != indices.end() && (v[i] != target || indices[target].size() > 1)) {
+                answers.push_back(i + 1);
+                res++;
+            }
+        }
+    }
+
+    cout << res << "\n";
+
+    for (int i: answers) {
+        cout << i << " ";
+    }
+}
--- Codeforces/1300/1119B.cpp
+++ Codeforces/1300/1119B.cpp
@@ -19,3 +19,36 @@ h height of fridge
 I think its a binary search qns
 u have to pair 2 closest bottles together to save space
 */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    long long h;
+    cin >> n >> h;
+
+    vector<long long> prefix{};
+    int res = 0;
+    
+    for (int i = 0; i < n; i++) {
+        long long x; 
+        cin >> x;
+        prefix.push_back(x);
+        sort(prefix.begin(), prefix.end(), greater<int>());
+
+        long long reqHeight = 0;
+        for (int j = 0; j < prefix.size(); j += 2) {
+            reqHeight += prefix[j];
+        }
+
+        if (reqHeight > h) {
+            cout << res;
+            return 0;
+        }
+        res++;
+    }
+    cout << res;
+}
