f9a96fa0a6ac1659424cac527391af4b29eb1c82
LohYangXian <lohyangxian@gmail.com>
2026-08-16 11:25:17 +0800

    started another question but not done

---
+++ Codeforces/1400/1475C.cpp
@@ -0,0 +1,23 @@
+//Ball in Berland
+
+/*
+pairs of boys and girls
+
+a boys and b girls
+
+k possible pairs
+
+distinct
+
+boys number first, then girls number
+
+find number of ways to select two pairs that match the condition above
+
+t cases
+a boys, b girls, k pairs
+
+k integers is number of boy in pair with number i
+k integers number of girl in pair with number i
+
+try O(n) soln
+*/
